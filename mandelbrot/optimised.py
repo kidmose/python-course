3567355148cecf4b9742c605b257ebe0f7410481
Egon Kidmose <kidmose@gmail.com>
2015-08-03 15:36:46 +0200

    optimisation step 02

--- mandelbrot/optimised.py
+++ mandelbrot/optimised.py
@@ -11,18 +11,28 @@ class OptimisedCalculator(mandelbrot.MandelbrotCalculator):
     Profiled using line_profiler with default parameters
     (I=100, Pim=100, Pre=100, T=10, naive=False, optimised=True,
     output='output', pim_max=1.5, pim_min=-1.5, pre_max=1.0, pre_min=-2.0)
-    Total running time before optimisation: 3.397 s
+    calculate() running time before optimisation: 3.397 s
 
-    # Optimisation step 1
+    # Optimisation step 01
     ## Observation
     `while math.sqrt(abs(z)) <= self.T and i < self.T:` is hit 245245 times
     and consumes 36.0% of the time.
     ## Change
     Eliminate math.sqrt by taking the square on both sides,
     as this is assumed to be an expensive operation.
-    ## Speedup
-    Negligeable, within noise.
+    ## Speed-up
+    Negligible, within noise.
     `**` operator might be as expensive as `math.sqrt`.
+
+    # Optimisation step 02
+    ## Observation
+    Step 01 provided no improvement. It is possible that another operation is the expensive one.
+    ## Change
+    On same line as targeted in step 01 the calculation of the square can be moved
+    to a precomputation step, eliminating the need to redo it 245245 times.
+    ## Speed-up
+    Still negligible.
+    Apparently the comparissons and/or the variable dereference is what takes the time.
     """
 
     file_name_data = "optimised_data.csv"
@@ -36,6 +46,8 @@ class OptimisedCalculator(mandelbrot.MandelbrotCalculator):
         re_span = self.pre_max-self.pre_min
         re_step = re_span / self.Pre
 
+        Tprecomp = self.T**2
+
         for i_im in range(self.Pim):
             im = i_im*im_step + self.pim_min
             row = list()
@@ -43,7 +55,7 @@ class OptimisedCalculator(mandelbrot.MandelbrotCalculator):
                 c = i_re*re_step + self.pre_min + im*1j
                 i = 0
                 z = 0+0j
-                while abs(z) <= self.T**2 and i < self.I:
+                while abs(z) <= Tprecomp and i < self.I:
                     z = z**2 + c
                     i += 1
                 row.append(i/self.I)
