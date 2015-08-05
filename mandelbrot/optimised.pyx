8c8bb068bd146e2b5cd03a2bb67980577552d3b8
Egon Kidmose <kidmose@gmail.com>
2015-08-05 09:28:14 +0200

    optimisation step 04
    declare all variables to cython

--- mandelbrot/optimised.pyx
+++ mandelbrot/optimised.pyx
@@ -14,34 +14,52 @@ class OptimisedCalculator(mandelbrot.MandelbrotCalculator):
     calculate() running time before optimisation: 3.397 s
 
     # Optimisation step 01
+
     ## Observation
+
     `while math.sqrt(abs(z)) <= self.T and i < self.T:` is hit 245245 times
     and consumes 36.0% of the time.
+
     ## Change
+
     Eliminate math.sqrt by taking the square on both sides,
     as this is assumed to be an expensive operation.
+
     ## Speed-up
+
     Negligible, within noise.
     `**` operator might be as expensive as `math.sqrt`.
 
     # Optimisation step 02
+
     ## Observation
+
     Step 01 provided no improvement. It is possible that another operation is the expensive one.
+
     ## Change
+
     On same line as targeted in step 01 the calculation of the square can be moved
     to a precomputation step, eliminating the need to redo it 245245 times.
+
     ## Speed-up
+
     Still negligible.
     Apparently the comparissons and/or the variable dereference is what takes the time.
 
     # Optimisation step 03
+
     ## Observation
+
     To cut down the most expensive part - the while-loop with comparisson and multipe references -
     at least two potential approaches exist:
     Vectorise to eliminate looping or compile to make operations cheaper.
+
     ## Change
+
     We use cython to translate the code into C which is then compiled.
+
     ## Speed-up
+
     `line_profiler` can no longer be used.
     We use `timeit` within ipython to compare against the naive implementation and
     see aproximately 2 times speed-up:
@@ -54,35 +72,65 @@ class OptimisedCalculator(mandelbrot.MandelbrotCalculator):
     >>> %timeit OC(I=100, Pim=100, Pre=100, T=10, output='output', pim_max=1.5, pim_min=-1.5, pre_max=1.0, pre_min=-2.0).calculate()
     10 loops, best of 3: 81.5 ms per loop
 
-    After compilation we rely on cythons annotation for a "profile",
-    as the compilation obfuscates the output of cProfile making it hard to use
+    After compilation we rely on cythons annotation for a "profile" (`cython -a mandelbrot/optimised.pyx`),
+    as the compilation obfuscates the output of cProfile making it hard to use,
     while line_profiler cannot be used at all.
+    What this really profiles is how much each line still needs to interact with the python interpreter, rather than running in pure c. 
+
+    # Optimisation step 04
+
+    ## Observation
+
+    Cython still makes many calls to python, as evident from the `profiling/03-step.html`.
+
+    ## Change
+
+    All variables used in the calculation loops are declared for cython to eliminate calls into python.
+
+    ## speed-up
+
+    >>> %timeit OC(I=100, Pim=100, Pre=100, T=10, output='output', pim_max=1.5, pim_min=-1.5, pre_max=1.0, pre_min=-2.0).calculate()
+    10 loops, best of 3: 36 ms per loop
+
     """
 
     file_name_data = "optimised_data.csv"
     file_name_plot = "optimised_plot.pdf"
 
     def calculate(self):
+        # Decalaring variables for cython
+        cdef double im_span, im_step, re_span, re_step
+        cdef unsigned long Tprecomp
+        cdef unsigned int i_im, i_re, Pim, Pre, i, I
+        cdef double im, re, pim_min, pre_min
+        cdef double complex z, c
+
+        # Avoiding object reference for constants
+        Tprecomp = self.T**2 # .. eliminate square root
+        Pim = self.Pim
+        Pre = self.Pre
+        pim_min = self.pim_min
+        pre_min = self.pre_min
+        I = self.I
+
         ms = list()
 
         im_span = self.pim_max-self.pim_min
-        im_step = im_span / self.Pim
+        im_step = im_span / Pim
         re_span = self.pre_max-self.pre_min
-        re_step = re_span / self.Pre
+        re_step = re_span / Pre
 
-        Tprecomp = self.T**2
-
-        for i_im in range(self.Pim):
-            im = i_im*im_step + self.pim_min
+        for i_im in range(Pim):
+            im = i_im*im_step + pim_min
             row = list()
-            for i_re in range(self.Pre):
-                c = i_re*re_step + self.pre_min + im*1j
+            for i_re in range(Pre):
+                c = i_re*re_step + pre_min + im*1j
                 i = 0
                 z = 0+0j
-                while abs(z) <= Tprecomp and i < self.I:
+                while abs(z) <= Tprecomp and i < I:
                     z = z**2 + c
                     i += 1
-                row.append(i/self.I)
+                row.append(float(i)/float(I))
             ms.append(row)
 
         return ms
